0da2774dba41d15a022740a6d0ac13ba09ae8b0e
Jennifer Duey <jlduey@gmail.com>
2019-08-28 03:37:49 -0400

    removing avatar

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -8,12 +8,12 @@ const ProfileTop = ({
         location,
         website,
         social,
-        user: { name, avatar }
+        user: { name, profilePhotoURL }
     }
 }) => {
     return (
         <div className='profile-top bg-primary p-2'>
-            <img className='round-img my-1' src={avatar} alt='' />
+            <img className='round-img my-1' src={profilePhotoURL} alt='' />
             <h1 className='large'>{name}</h1>
             <p className='lead'>
                 {status} {company && <span> at {company}</span>}
